e1d9b53f1e08a1330f0a88665d2f5d6659455963
Fenix00708 <Fenix00708@gmail.com>
2022-01-30 18:38:10 +0200

    создал проверку кнопки

--- baza.php
+++ baza.php
@@ -9,16 +9,27 @@ if ($name == 'Сергей') {
 } else {
     echo 'Вы не Сергей';
 }
+echo "</br>";
+
+if (isset($_POST["bot"])) {
+    echo 'Вы не бот';
+} else {
+    echo 'Вы бот';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="registration.html"><button type="submit">Назад</button></form>
 </body>
+
 </html>
